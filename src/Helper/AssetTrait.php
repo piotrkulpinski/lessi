3878c6c276812e614ce3c1b6d87aa52f3b6019df
Piotr Kulpinski <piotr@kulpinski.pl>
2022-08-02 12:32:33 +0200

    Add better docs to the AssetTrait and improve some of the methods

--- src/Helper/AssetTrait.php
+++ src/Helper/AssetTrait.php
@@ -13,44 +13,47 @@ trait AssetTrait {
 	/**
 	 * Stored manifest JSON file
 	 *
-	 * @var array
+	 * @var array|null
 	 */
-	private $manifest;
+	private ?array $manifest;
 
 	/**
 	 * Returns the real path of the asset file.
 	 *
-	 * @param $asset
+	 * @param string $asset Path to the asset file
 	 *
 	 * @return string
 	 */
-	public function assetUrl( $asset ) {
+	public function assetUrl( string $asset ): string {
 		return $this->revisionedUrl( $this->getPath( config()->getAssetsPath(), $asset ) );
 	}
 
 	/**
 	 * Verifies existence of the given file in manifest
 	 *
+	 * @param string $asset Path to the asset file
+	 * @param array|null $manifest Optional. Already fetched manifest object
+	 *
 	 * @return bool
 	 */
-	protected function hasFile( $asset ) {
-		$manifest = $this->getManifest();
+	protected function hasFile( string $asset, ?array $manifest = null ): bool {
+		$manifest ??= $this->getManifest();
 
-		return array_key_exists( $asset, $manifest );
+		return is_array( $manifest ) && array_key_exists( $asset, $manifest );
 	}
 
 	/**
 	 * Returns the real url of the revisioned file.
 	 * based on the manifest file content.
 	 *
-	 * @param $asset
+	 * @param string $asset Path to the asset file
 	 *
 	 * @return string
 	 */
-	protected function revisionedUrl( $asset ) {
+	protected function revisionedUrl( string $asset ): string {
 		$manifest = $this->getManifest();
 
-		if ( ! array_key_exists( $asset, $manifest ) ) {
+		if ( ! $this->hasFile( $asset, $manifest ) ) {
 			return 'FILE-NOT-REVISIONED';
 		}
 
@@ -60,18 +63,18 @@ trait AssetTrait {
 	/**
 	 * Checks if request is in development environment
 	 *
-	 * @return boolean
+	 * @return bool
 	 */
-	private function isDev() {
+	private function isDev(): bool {
 		return defined( 'THEME_DEV_ENV' );
 	}
 
 	/**
 	 * Get parsed manifest file content
 	 *
-	 * @return array
+	 * @return array|null
 	 */
-	private function getManifest() {
+	private function getManifest(): ?array {
 		if ( empty( $this->manifest ) ) {
 			$this->manifest = $this->fetchManifest();
 		}
@@ -81,16 +84,20 @@ trait AssetTrait {
 
 	/**
 	 * Fetches data from remote manifest file
+	 *
+	 * @return array|null
 	 */
-	private function fetchManifest() {
-		$manifestPath = $this->isDev()
-			? config()->getManifestDevPath()
-			: config()->getManifestPath();
+	private function fetchManifest(): ?array {
+		$manifestPath = $this->getTemplatePath(
+			$this->isDev()
+				? config()->getManifestDevPath()
+				: config()->getManifestPath()
+		);
 
-		$response = wp_remote_get( $this->getTemplateUrl( $manifestPath ) );
-
-		if ( ! is_wp_error( $response ) && 200 === wp_remote_retrieve_response_code( $response ) ) {
-			return (array) json_decode( $response['body'] );
+		if ( file_exists( $manifestPath ) ) {
+			return (array) json_decode( file_get_contents( $manifestPath ) );
 		}
+
+		return null;
 	}
 }
